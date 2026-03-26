333a6362980ba0413879ceb4e8d7141445f5f6b4
dabin.labpc <kwondab@gmail.com>
2026-03-26 18:37:42 +0000

    update spec.py (incomplete)

--- projects/trial_to_trial_variability/spec.py
+++ projects/trial_to_trial_variability/spec.py
@@ -243,7 +243,6 @@ def load_and_process_data(
     target = np.stack([Y_train, Y_test], axis=0)  # (2, n_target, T)
     return {"stimulus" : angles, "source": source, "target": target}
 
-
 def train_test_split(
     X: Dict[str, np.ndarray],
     # -- ALL SUBSEQUENT PARAMS MUST BE SPECIFIED IN THE CONFIG FILE ---
@@ -286,27 +285,246 @@ def train_test_split(
 # ========================
 
 def model_v1(X, params):
-    raise NotImplementedError
+    """ Gain Modulation + per cell modulation
 
+    Equation : For each target cell c at timepoint t with stimulus angle theta, 
+        f(theta, t; cell_params) = multiplicative_gain(t) * g(theta(t) ; cell_params) + additive_offset(t) * coupling_factor
+    where g(theta(t); cell_params) is some tuning function. 
+
+    Args : 
+        X (dict) : Inputs object with keys 'stimulus', neural responses with shape (n_source_cells + 1 , n_time). You can get the neural responses only by removing the 'stimulus' feature from X. 
+        params (dict) : Parameter dictionary with keys: 
+            - source_tuning_params : parameters for the tuning function g(theta) (shape (n_source_cells, n_params))
+            - source_coupling_factor : coupling_factor (shape (n_source_cells,)) 
+            - target_tuning_params : parameters for the tuning function g(theta) (shape (n_target_cells, n_params))
+            - target_coupling_factor : coupling_factor (shape (n_target_cells,)) 
+
+    Returns : 
+        jnp.ndarray : Predicted responses for the target cells with shape (n_target_cells, n_time)
+    """
+    stimuli = jnp.array(X['stimulus']) # shape (n_time)
+    source_response = jnp.array(X.remove_feature('stimulus')) # shape (n_source, n_time)
+
+    source_tuning_params = params['source_tuning_params'] # shape (n_source_cells, n_params)
+    source_coupling_factor = params['source_coupling_factor'] # shape (n_source_cells,)
+    target_tuning_params = params['target_tuning_params'] # shape (n_target_cells, n_params)
+    target_coupling_factor = params['target_coupling_factor'] # shape (n_target_cells,)    
+
+    source_stimuli = jnp.tile(stimuli[None, :], (source_tuning_params.shape[0], 1)) # shape (n_source, n_time)
+    g_source = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(source_stimuli, source_tuning_params) # shape (n_source, n_time)
+
+    eps = 1e-8
+    multiplicative_gain = jnp.sum(g_source * source_response, axis=0) / (jnp.sum(g_source**2, axis=0) + eps) # shape (n_time,)
+
+    source_residual = source_response.T - (multiplicative_gain[:, None] * g_source.T) # shape (n_time, n_source)
+    # we already know the source_coupling_factor
+    additive_offset = jnp.sum(source_residual * source_coupling_factor[None, :], axis=1) / (jnp.sum(source_coupling_factor**2) + eps) # shape (n_time,)
+
+    target_stimuli = jnp.tile(stimuli[None, :], (target_tuning_params.shape[0], 1)) # shape (n_target, n_time)
+    g_target = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(target_stimuli, target_tuning_params).T # shape (n_time, n_target)
+    pred = multiplicative_gain[:, None] * g_target + additive_offset[:, None] * target_coupling_factor[None, :]
+    # clip to non-negative firing rates
+    pred = jnp.clip(pred, a_min=0.0)
+    return pred.T
 
 def param_est_v1(X, Y):
-    raise NotImplementedError
+    """ Parameter estimator for model_v1. This function estimates params and from the data.
+    Tuning_params : contain per cell level parameters for both source and target cells that are independent of trials
 
+    Args :
+        X (jnp.ndarray) : Input array of Training responses of source cells with shape (n_source_cells + 1 , n_time), including an angle feature. You can get the neural responses only by removing the 'stimulus' feature from X.
+        Y (jnp.ndarray) : Output array of Training responses of target cells with shape (n_target_cells + 1, n_time), including an angle feature. You can get the neural responses only by removing the 'stimulus' target from Y.
+        neuron_model (function) : Tuning function to fit for each cell. Should take in stimulus and cell-specific parameters and output predicted response.
+        parameter_estimator_jax (function) : Function to estimate tuning parameters for each cell given the stimulus and responses. Should return parameters in the correct order for the neuron_model. Should be JAX-compatible for use in gradient-based optimization.
+    
+    Returns :
+        params (dict) : Estimated tuning parameters for target cells. Keys:
+            - source_tuning_params : shape (n_source_cells, n_params)
+            - source_cell_coupling_factor : shape (n_source_cells,)
+            - target_tuning_params : shape (n_target_cells, n_params)
+            - target_cell_coupling_factor : shape (n_target_cells,)
+    """
+    # first sort the response of x and y by the stimulus angles 
+    stims = X['stimulus'][0] # shape (n_time,)
+    x = jnp.array(X.remove_feature('stimulus')) # shape (n_source, n_time)
+    y = jnp.array(Y.remove_target('stimulus')) # shape (n_target, n_time)
+
+    stims_idx = jnp.argsort(stims)
+    stims = stims[stims_idx]
+    x = x[:, stims_idx]
+    y = y[:, stims_idx]
+
+    # first calculate the tuning parameters from the "mean responses" which is the binned mean response of angle bins. 
+    n_bins = 256
+    bin_edges = jnp.linspace(0, 2 * jnp.pi, n_bins + 1)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    bin_idx = jnp.digitize(stims, bin_edges) - 1
+    bin_idx = jnp.clip(bin_idx, 0, n_bins - 1)
+    
+    x_binned_spike_counts = jnp.zeros((x.shape[0], n_bins)).at[:, bin_idx].add(x)
+    x_binned_counts = jnp.zeros(n_bins).at[bin_idx].add(1.0)
+    x_binned_spike_rate = jnp.zeros_like(x_binned_spike_counts).at[:, :].set(jnp.where(x_binned_counts > 0, x_binned_spike_counts / x_binned_counts, 0.0))
+
+    y_binned_spike_counts = jnp.zeros((y.shape[0], n_bins)).at[:, bin_idx].add(y)
+    y_binned_counts = jnp.zeros(n_bins).at[bin_idx].add(1.0)
+    y_binned_spike_rate = jnp.zeros_like(y_binned_spike_counts).at[:, :].set(jnp.where(y_binned_counts > 0, y_binned_spike_counts / y_binned_counts, 0.0))
+
+    # Step 1 : For every source cell, fit a peaky tuning curve 
+    source_params_init = jax.vmap(parameter_estimator_jax, in_axes=(None, 0))(bin_centers, x_binned_spike_rate) # shape (n_source, n_params)
+    # use gradient descent to optimise the tuning parameters 
+    source_tuning_params = _optimize_params(source_params_init, stims, x) 
+
+    target_params_init = jax.vmap(parameter_estimator_jax, in_axes=(None, 0))(bin_centers, y_binned_spike_rate) # shape (n_target, n_params)
+    target_tuning_params = _optimize_params(target_params_init, stims, y) 
+
+    # Step 2 : Fit the gain factor using leastsq 
+    source_stimuli = jnp.tile(stims[None, :], (source_tuning_params.shape[0], 1)) # shape (n_source, n_time)
+    g_source = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(source_stimuli, source_tuning_params) # shape (n_source, n_time)
+
+    n_source, n_t = g_source.shape
+
+    eps = 1e-8
+    multiplicative_gain = jnp.sum(g_source * x, axis=0) / (jnp.sum(g_source**2, axis=0) + eps)
+
+    # Step 3 : Fit a rank 1 model to the residual using SVD 
+    residual = x.T - (multiplicative_gain[:, None] * g_source.T) # has shape (n_time, n_source)
+    U, S, Vh = jnp.linalg.svd(residual, full_matrices=False)
+    source_coupling_factor = Vh[0, :] # shape (n_source,)
+    additive_offset = U[:, 0] * S[0] # shape (n_time,)
+
+    # Step 4 : Fit the target cell coupling factor using the multiplicative_gain and additive offset
+    n_target = target_tuning_params.shape[0]
+    target_stims = jnp.tile(stims[None, :], (n_target, 1)) # shape (n_target, n_time)
+    g_target = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(target_stims, target_tuning_params) # shape (n_target, n_time)
+
+    multiplicative_only_pred = multiplicative_gain[:, None] * g_target.T # shape (n_time, n_target)
+    residual_target = y.T - multiplicative_only_pred # shape (n_time, n_target
+    target_coupling_factor = (residual_target.T @ additive_offset) / (additive_offset @ additive_offset) # shape (n_target,)
+
+    params = {
+        'source_tuning_params' : source_tuning_params,
+        'source_coupling_factor' : source_coupling_factor,
+        'target_tuning_params' : target_tuning_params,
+        'target_coupling_factor' : target_coupling_factor
+    }
+    return params
 
 def model_v2(X, params):
-    raise NotImplementedError
+    """ Gain Modulation + source to target coupling 
 
+    Equation : For each target cell c at timepoint t with stimulus angle theta, 
+        f(theta, t; cell_params) = multiplicative_gain(t) * g(theta(t) ; cell_params) + source_cell_response(t) * coupling_weight
+    where g(theta(t); cell_params) is some tuning function, source_cell_response(t) is the response of the source cell at time t (shape n_source,) and the coupling weight is the coupling factor for each target cell (shape n_source,).
+
+    Args : 
+        X (dict) : Inputs object with keys 'stimulus', neural responses with shape (n_source_cells + 1 , n_time). You can get the neural responses only by removing the 'stimulus' feature from X. 
+        params (dict) : Parameter dictionary with keys: 
+            - source_tuning_params : parameters for the tuning function g(theta) (shape (n_source_cells, n_params))
+            - target_tuning_params : parameters for the tuning function g(theta) (shape (n_target_cells, n_params))
+            - coupling_factor : coupling_factor (shape (n_target_cells, n_source_cells)) 
+
+    Returns : 
+        jnp.ndarray : Predicted responses for the target cells with shape (n_target_cells, n_time)
+    """
+    stimuli = jnp.array(X['stimulus']) # shape (n_time)
+    source_response = jnp.array(X.remove_feature('stimulus')) # shape (n_source, n_time)
+
+    source_tuning_params = params['source_tuning_params'] # shape (n_source_cells, n_params)
+    target_tuning_params = params['target_tuning_params'] # shape (n_target_cells, n_params)
+    coupling_factor = params['target_coupling_factor'] # shape (n_target_cells, n_source_cells)    
+
+    source_stimuli = jnp.tile(stimuli[None, :], (source_tuning_params.shape[0], 1)) # shape (n_source, n_time)
+    g_source = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(source_stimuli, source_tuning_params) # shape (n_source, n_time)
+
+    eps = 1e-8
+    multiplicative_gain = jnp.sum(g_source * source_response, axis=0) / (jnp.sum(g_source**2, axis=0) + eps) # shape (n_time,)
+
+    target_stimuli = jnp.tile(stimuli[None, :], (target_tuning_params.shape[0], 1)) # shape (n_target, n_time)
+    g_target = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(target_stimuli, target_tuning_params).T # shape (n_time, n_target)
+    pred = multiplicative_gain[:, None] * g_target + (coupling_factor @ source_response).T # shape (n_time, n_target)
+    # clip to non-negative firing rates
+    pred = jnp.clip(pred, a_min=0.0)
+    return pred.T
 
 def param_est_v2(X, Y):
-    raise NotImplementedError
+    """ Parameter estimator for model_v1. This function estimates params and from the data.
+    Tuning_params : contain per cell level parameters for both source and target cells that are independent of trials
 
+    Args :
+        stims (jnp.ndarray) : Stimulus angles with shape (n_time,)
+        x (jnp.ndarray) : Training responses of source cells with shape (n_source_cells, n_time)
+        y (jnp.ndarray) : Training responses of target cells with shape (n_target_cells, n_time)
+        neuron_model (function) : Tuning function to fit for each cell. Should take in stimulus and cell-specific parameters and output predicted response.
+        parameter_estimator_jax (function) : Function to estimate tuning parameters for each cell given the stimulus and responses. Should return parameters in the correct order for the neuron_model. Should be JAX-compatible for use in gradient-based optimization.
+    
+    Returns :
+        params (dict) : Estimated tuning parameters for target cells. Keys:
+            - source_tuning_params : shape (n_source_cells, n_params)
+            - target_tuning_params : shape (n_target_cells, n_params)
+            - coupling_factor : shape (n_target_cells, n_source_cells)
+    """
+    # first sort the response of x and y by the stimulus angles 
+    stims = X['stimulus'][0] # shape (n_time,)
+    x = jnp.array(X.remove_feature('stimulus')) # shape (n_source, n_time)
+    y = jnp.array(Y.remove_target('stimulus')) # shape (n_target, n_time)
+
+    stims_idx = jnp.argsort(stims)
+    stims = stims[stims_idx]
+    x = x[:, stims_idx]
+    y = y[:, stims_idx]
+
+    # first calculate the tuning parameters from the "mean responses" which is the binned mean response of angle bins. 
+    n_bins = 256
+    bin_edges = jnp.linspace(0, 2 * jnp.pi, n_bins + 1)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    bin_idx = jnp.digitize(stims, bin_edges) - 1
+    bin_idx = jnp.clip(bin_idx, 0, n_bins - 1)
+    
+    x_binned_spike_counts = jnp.zeros((x.shape[0], n_bins)).at[:, bin_idx].add(x)
+    x_binned_counts = jnp.zeros(n_bins).at[bin_idx].add(1.0)
+    x_binned_spike_rate = jnp.zeros_like(x_binned_spike_counts).at[:, :].set(jnp.where(x_binned_counts > 0, x_binned_spike_counts / x_binned_counts, 0.0))
+
+    y_binned_spike_counts = jnp.zeros((y.shape[0], n_bins)).at[:, bin_idx].add(y)
+    y_binned_counts = jnp.zeros(n_bins).at[bin_idx].add(1.0)
+    y_binned_spike_rate = jnp.zeros_like(y_binned_spike_counts).at[:, :].set(jnp.where(y_binned_counts > 0, y_binned_spike_counts / y_binned_counts, 0.0))
+
+    # Step 1 : For every source cell, fit a peaky tuning curve 
+    source_params_init = jax.vmap(parameter_estimator_jax, in_axes=(None, 0))(bin_centers, x_binned_spike_rate) # shape (n_source, n_params)
+    # use gradient descent to optimise the tuning parameters 
+    source_tuning_params = _optimize_params(source_params_init, stims, x) 
+
+    target_params_init = jax.vmap(parameter_estimator_jax, in_axes=(None, 0))(bin_centers, y_binned_spike_rate) # shape (n_target, n_params)
+    target_tuning_params = _optimize_params(target_params_init, stims, y) 
+
+    # Step 2 : Fit the gain factor using leastsq 
+    source_stimuli = jnp.tile(stims[None, :], (source_tuning_params.shape[0], 1)) # shape (n_source, n_time)
+    g_source = jax.vmap(lambda stim, params: neuron_model(stim, *params), in_axes=(0, 0))(source_stimuli, source_tuning_params) # shape (n_source, n_time)
+
+    n_source, n_t = g_source.shape
+
+    eps = 1e-8
+    multiplicative_gain = jnp.sum(g_source * x, axis=0) / (jnp.sum(g_source**2, axis=0) + eps)
+
+    # Step 3 : Fit the coupling factor by regressing the residual against the source cell responses
+    residual = x.T - (multiplicative_gain[:, None] * g_source.T) # has shape (n_time, n_source)    
+    coupling_factor = jnp.linalg.lstsq(y, residual, rcond=None)[0].T # shape (n_target, n_source)
+
+    params = {
+        'source_tuning_params' : source_tuning_params,
+        'target_tuning_params' : target_tuning_params,
+        'coupling_factor' : coupling_factor
+    }
+    return params
 
 # ========================
 # 3. LOSS
 # ========================
 
 def loss_fn(Y_pred, Y_true):
-    raise NotImplementedError
+    """
+    Elementwise squared-error loss.
+    """
+    return (Y_true - Y_pred) ** 2
 
 
 # ========================
